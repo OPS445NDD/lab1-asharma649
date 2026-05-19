cb2054f0a84ce8c07d3a042c944c6df198f83b67
Arman Sharma <asharma649@myseneca.ca>
2026-05-19 14:16:47 -0400

    completed lab1

--- lab1d.py
+++ lab1d.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+'''Description: This program calculates a math expression.'''
 
 x = 10
 y = 2
 z = 5
 
-print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(x + y * z))
+answer = x + y * z
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(answer))
